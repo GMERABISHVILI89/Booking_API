99c0488d6c5ee1d73e8323c9447089dd42b3e132
giorgi.merabishvili <g.merabishvili@rs.ge>
2025-03-21 11:23:14 +0400

    seed-roomtypes

--- Booking_API/Models/ApplicationDbContext.cs
+++ Booking_API/Models/ApplicationDbContext.cs
@@ -25,6 +25,14 @@ namespace Booking_API.Models
             modelBuilder.Entity<Room>().HasKey(r => r.Id);
             modelBuilder.Entity<BookedDate>().HasKey(b => b.Id);
             modelBuilder.Entity<Image>().HasKey(i => i.Id);
+
+   //         modelBuilder.Entity<RoomType>().HasData(
+   //    new RoomType { Id = 1, TypeName = "Single Room" },
+   //    new RoomType { Id = 2, TypeName = "Double Room" },
+   //    new RoomType { Id = 3, TypeName = "Triple Room" },
+   //    new RoomType { Id = 4, TypeName = "Deluxe Room" },
+   //    new RoomType { Id = 5, TypeName = "Family Room" }
+   //);
         }
 
     }
